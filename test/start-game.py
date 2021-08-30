bcff90d05fcd4bb7e8ab7a763f03969f660abac5
James Pedersen <james00090@gmail.com>
2021-08-29 23:20:38 -0700

    Changed path in test/start-game.py

--- test/start-game.py
+++ test/start-game.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 import time
 
-proc = subprocess.Popen(["C:\workspace\\go-jenkins\\bin\\tic-tac-toe.exe"], shell=True)
+proc = subprocess.Popen(["/home/ubuntu/tic-tac-toe/bin/tic-tac-toe"], shell=True)
 time.sleep(3)
 pid = proc.pid
 print('pid = ', pid)
