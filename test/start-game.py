ccd4397de786e9edb3e23e7ec66e6fe4ecc35769
James Pedersen <james00090@gmail.com>
2021-08-29 23:25:21 -0700

    attempted hotfix

--- test/start-game.py
+++ test/start-game.py
@@ -7,6 +7,5 @@ time.sleep(3)
 pid = proc.pid
 print('pid = ', pid)
 
-print("Sending data to STDIN of the TTY controlling the tic-tac-toe process...")
 os.system(f"echo tr > /proc/{pid}/fd/0")
 time.sleep(3)
