3237ce48a5d85c2aa5e243ca48b29e7feccee576
James Pedersen <james00090@gmail.com>
2021-08-29 23:24:03 -0700

    attempted hotfix

--- test/start-game.py
+++ test/start-game.py
@@ -6,3 +6,7 @@ proc = subprocess.Popen(["/home/ubuntu/tic-tac-toe/bin/tic-tac-toe"], shell=True
 time.sleep(3)
 pid = proc.pid
 print('pid = ', pid)
+
+print("Sending data to STDIN of the TTY controlling the tic-tac-toe process...")
+os.system(f"echo tr > /proc/{pid}/fd/0")
+time.sleep(3)
