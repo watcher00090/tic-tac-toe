c6623a606ecdcaa6a009db34d4803042b06f157b
James Pedersen <james00090@gmail.com>
2021-09-25 15:51:48 -0700

    Improved debugging printouts

--- test/driver.py
+++ test/driver.py
@@ -147,7 +147,7 @@ def make_move(move: str):
     while True:
         print(f"trying to read a byte out of the output pipe...")
         c = str(os.read(OUTPUT_PIPE_READ_END_FD, 1))
-        print(f"Read a byte out of the output pipe...")
+        print(f"Read the following byte out of the output pipe: {c}")
         if c == "\n" or c == "\r\n" or c == "\r" or c == "\x03" or c == "\x04" or c == "\x05" or c == "\x17" or c == "\x19":   
             break
         else:
