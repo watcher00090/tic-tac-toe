1d6a73c01069275e28ddb8787a74a8c782320d25
James Pedersen <james00090@gmail.com>
2021-09-24 18:31:32 -0700

    Changed to debug mode

--- test/driver.py
+++ test/driver.py
@@ -146,10 +146,10 @@ def make_move(move: str):
 
     STDIN_PIPE_WRITE_END_FILEHANDLE.write(move)
     print(f"Successfully pushed the move into the pipe...")
-    output_str = OUTPUT_PIPE_READ_END_FILEHANDLE.readline()
-    print(f"Successfully got the next line of output from the output pipe...")
-    OUTPUT_FILE.write(output_str)
-    print(f"Successfully wrote the next line of output to the output file....")
+    for l in OUTPUT_PIPE_READ_END_FILEHANDLE
+        print(f"Successfully got the next line of output from the output pipe...")
+        OUTPUT_FILE.write(l)
+        print(f"Successfully wrote the next line of output to the output file....")
 
         # stderr_chunks = stderr_str.splitlines()
         # stdout_chunks = stdout_str.splitlines()
